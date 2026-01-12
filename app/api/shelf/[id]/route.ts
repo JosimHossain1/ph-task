fe2a69af27d81c1205bcfd8efa952a9d185f6736
JosimHossain1 <josimhossain842@gmail.com>
2026-01-12 19:22:40 +0600

    DEvelop shelf API

---
+++ app/api/shelf/[id]/route.ts
@@ -0,0 +1,32 @@
+import dbConnect from "@/lib/dbConnect";
+import shelfModel from "@/models/shelfModel";
+import { NextRequest } from "next/server";
+
+export async function GET(req: NextRequest, { params }: { params: { id: string } }) {
+  await dbConnect();
+
+  const { id } = await params;
+
+  const findSingleShelf = await shelfModel.findById(id)
+  return new Response(JSON.stringify({ message: "Shelf Goted", data: findSingleShelf }), { status: 200 })
+}
+
+export async function DELETE(req: NextRequest, { params }: { params: { id: string } }) {
+  await dbConnect();
+
+  const { id } = await params;
+
+  const deletedShelf = await shelfModel.findByIdAndDelete(id)
+  return new Response(JSON.stringify({ message: "Shelf Deletd", data : deletedShelf}), { status: 204 })
+}
+
+
+export async function PUT(req: NextRequest, { params }: { params: { id: string } }) {
+  await dbConnect();
+
+  const { id } = await params;
+  const body = await req.json()
+
+  const updatedShelf = await shelfModel.findByIdAndUpdate(id, {$set : body}, {new : true})
+  return new Response(JSON.stringify({ message: "Shelf Updated", data : updatedShelf }), { status: 200 })
+}

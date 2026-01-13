79043f7a5722addac8b1dcf2dee9a8834911703a
JosimHossain1 <josimhossain842@gmail.com>
2026-01-13 19:18:38 +0600

    Fix the params problem

--- app/api/books/[id]/route.ts
+++ app/api/books/[id]/route.ts
@@ -2,29 +2,33 @@ import dbConnect from "@/lib/dbConnect";
 import bookModel from "@/models/bookModel";
 import { NextRequest } from "next/server";
 
-export async function GET(req: NextRequest, { params }: { params: { id: string } }) {
+type Params = {
+  params: Promise<{ id: string }>
+}
+
+export async function GET(req: NextRequest, context: Params) {
   await dbConnect();
 
-  const { id } = await params;
+  const { id } = await context.params;
 
   const findSingleBook = await bookModel.findById(id)
   return new Response(JSON.stringify({ message: "Book Goted", data: findSingleBook }), { status: 200 })
 }
 
-export async function DELETE(req: NextRequest, { params }: { params: { id: string } }) {
+export async function DELETE(req: NextRequest, context: Params) {
   await dbConnect();
 
-  const { id } = await params;
+  const { id } = await context.params;
 
   const deletedBook = await bookModel.findByIdAndDelete(id)
   return new Response(JSON.stringify({ message: "Book Deletd", data: deletedBook }), { status: 200 })
 }
 
 
-export async function PUT(req: NextRequest, { params }: { params: { id: string } }) {
+export async function PUT(req: NextRequest, context: Params) {
   await dbConnect();
 
-  const { id } = await params;
+  const { id } = await context.params;
   const body = await req.json()
 
   const updatedBook = await bookModel.findByIdAndUpdate(id, { $set: body }, { new: true })
